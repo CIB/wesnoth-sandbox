823b877f8ada21f477b5bb9b86a4b0b652ed522f
cib <cib123@googlemail.com>
2013-01-21 18:02:25 +0100

    Added faction datums.

---
+++ lua/faction.lua
@@ -0,0 +1,45 @@
+factions = { }
+
+factions["Humans"] = {
+	id = "Humans",
+	name = _ "Human Empire",
+	relations = {
+		Humans = 100,
+		Elves = 0,
+		Dwarves = 0,
+		Bandits = -100
+	}
+}
+
+factions["Elves"] = {
+	id = "Elves",
+	name = _ "Elvish Alliance",
+	relations = {
+		Humans = 0,
+		Elves = 100,
+		Dwarves = -50,
+		Bandits = -50
+	}
+}
+
+factions["Dwarves"] = {
+	id = "Dwarves",
+	name = _ "Dwarven Kingdom",
+	relations = {
+		Humans = 0,
+		Elves = -50,
+		Dwarves = 100,
+		Bandits = -50
+	}
+}
+
+factions["Bandits"] = {
+	id = "Bandits",
+	name = _ "Outlaws",
+	relations = {
+		Humans = -50,
+		Elves = -50,
+		Dwarves = -50,
+		Bandits = 100
+	}
+}
